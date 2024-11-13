88b3eb6a58d71010da588dd4a1034ebcd27fe3f4
Jason20668 <526JBach@frhsd.com>
2024-11-13 09:30:05 -0500

    finished interpolation probably

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ let myStory = `One ${Weather} ${Day} James Carmicheal was getting ready for scho
 She had a floppy ${Color} hat and was ${GnoNum} inches tall. She said in a ${VoiAdj} voice, "My name is ${Name} and we need your help James Carmicheal. 
 Your the only person short enough to be able to go through the portal and save ${Town}!". James happily agreed and ${FirVerb} through the portal to ${Town}. 
 When he got there we was surrounded by ${GnoAmount} gnomes all of which looked very ${Emotion}. He was told that they wanted to make a ${BakedG} for the festival today
-but the ${BakedI} was on the top shelf and they hadn't invented stepstools yet. Luckily, James was (height) feet tall and was barely able to reach the shelf. 
-All the gnomes (positive verb) as they could now make their (baked good) and gave James a (noun) as a reward. They told James he was welcome any time in (town name) and
-that he was a friend of the gnomes for (time). James then once again (verb) through the portal to get home and get ready for school. 
-Unfortunately he was (adjective) from the adventure and passed out, missing his very important (school subject) class. Poor James.`;
+but the ${BakedI} was on the top shelf and they hadn't invented stepstools yet. Luckily, James was ${JamesHei} feet tall and was barely able to reach the shelf. 
+All the gnomes ${PosVer} as they could now make their ${Reward} and gave James a ${Reward} as a reward. They told James he was welcome any time in ${Town} and
+that he was a friend of the gnomes for ${Time}. James then once again ${SecVerb} through the portal to get home and get ready for school. 
+Unfortunately he was ${Adj} from the adventure and passed out, missing his very important ${Subject} class. Poor James.`;
 console.log(myStory);
 
 document.getElementById('story').innerHTML = myStory;
